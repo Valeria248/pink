b026a01526c13101df0178c12c0850990a0fe8a2
Valeria248 <82720192+Valeria248@users.noreply.github.com>
2021-05-07 13:33:15 +0300

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -43,6 +43,12 @@ racket1 = Player('racket.png', 30, 200, 4, 50, 150)
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+
 speed_x = 3
 speed_y = 3
 
@@ -52,10 +58,32 @@ while game:
             game = False
 
     if finish != True:
+        
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
         window.fill(back)
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        if ball.rect.y > win_height -50 or ball.rect.y < 0:
+            speed_y *= -1
+        
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+
+        
         racket1.update_l()
         racket2.update_r()
-
+        
         racket1.reset()
         racket2.reset()
         ball.reset()
